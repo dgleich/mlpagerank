aaed17b3774da1d99c940baad10d31eb7dd12f33
yuyongyang800 <yuyongyang800@gmail.com>
2013-08-29 16:25:12 -0400

    rho->kappa, flagr->flagk

--- tensorRank.m
+++ tensorRank.m
@@ -1,24 +1,25 @@
-function [xtrue, xhist, flag, sumx, diffsumx, rho, flagr] ...
+function [xtrue, xhist, flag, sumx, diffsumx, kappa, flagk] ...
     = tensorRank(alpha, R, v, x, gamma)
-% [xtrue, xhist, flag, sumx, diffsumx, rho, flagr] = tensorRank(alpha, R, v, x, gamma)
+% [xtrue, xhist, flag, sumx, diffsumx, kappa, flagr] = tensorRank(alpha, R, v, x, gamma)
 % xtrue is the true value of vector x (computed after nth iteration)
 % xhist records each iteration
 % sumx records the sum of vector x in each iteration
 % diffsumx records the difference between x_n and 1
-% rho records the ratio of ||x^(k+1) - x^(k)||_1 / ||x^(k) - x^(k-1)||_1
+% kappa records the ratio of ||x^(k+1) - x^(k)||_1 / ||x^(k) - x^(k-1)||_1
 % flag -- 0 converge;  flag -- 1, not converge
 % gamma -- shifting factor, default value is 0.5
+% flagk -- indicator for kappa > 1
 if nargin < 5
 	gamma = 0.5;
 end
 tol = 1e-12;
 niter = 10000;
 flag = 0; 
-flagr = 0;
+flagk = 0;
 xhist = zeros(size(x,1), niter);
 sumx = zeros(niter, 1);
 diffsumx = zeros(niter, 1);
-rho = zeros(niter - 2, 1);
+kappa = zeros(niter - 2, 1);
 xcur = x;
 Gamma = 1 / (1+gamma);
 for n = 1 : niter
@@ -33,9 +34,9 @@ for n = 1 : niter
     
     xhist(:, n) = xn;
     if n > 2
-            rho(n - 2) = norm(xhist(:,n) - xhist(:,n-1), 1) / norm(xhist(:,n-1) - xhist(:,n-2), 1);
-			if rho(n - 2) > 1
-				flagr = 1;
+            kappa(n - 2) = norm(xhist(:,n) - xhist(:,n-1), 1) / norm(xhist(:,n-1) - xhist(:,n-2), 1);
+			if kappa(n - 2) > 1
+				flagk = 1;
 			end
     end
     if norm(xn - xcur, inf) <= tol
@@ -43,7 +44,7 @@ for n = 1 : niter
         xhist = xhist(:, 1:n);
         sumx = sumx(1 : n);
         diffsumx = diffsumx(1:n);
-        rho = rho(1:n-2);
+        kappa = kappa(1:n-2);
         break;
     else
         xcur = xn;        
@@ -54,5 +55,5 @@ if size(xhist, 2) == niter
     xtrue = xhist(:, niter);
     flag = 1;
 end
-%valSol(alpha, R, xtrue, v);
+valSol(alpha, R, xtrue, v);
 end
