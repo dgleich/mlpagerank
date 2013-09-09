54e31129c0686692896de6b1fed4dfbb5b78eb94
yuyongyang800 <yuyongyang800@gmail.com>
2013-09-09 17:13:57 -0400

    change to old version

--- convertR2P.m
+++ convertR2P.m
@@ -6,9 +6,9 @@ P = zeros(n^2, n^2);
 for i = 1:n     % group i
     tmp = zeros(n^2, n);
     for j = 1:n % column j
-        ei = zeros(n, 1);
-        ei(i) = 1;
-        tmp(:, j) = kron(R(:, (i-1)*n + j), ei);
+        ej = zeros(n, 1);
+        ej(j) = 1;
+        tmp(:, j) = kron(ej, R(:, (i-1)*n + j));
     end
     P(:, (i-1)*n +1: i*n) = tmp;
 end
