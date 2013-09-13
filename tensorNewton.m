17f8a74e667f602548f6030cc9ec0a82314f543a
yuyongyang800 <yuyongyang800@gmail.com>
2013-09-13 14:02:45 -0400

    add optional arguments

--- tensorNewton.m
+++ tensorNewton.m
@@ -1,10 +1,21 @@
-function [xtrue, xhist, kappa] = tensorNewton(alpha, R, v, x)
+function [xtrue, xhist, kappa] = tensorNewton(alpha, R, v, x, maxIter, ntol)
+% function [xtrue, xhist, kappa] = tensorNewton(alpha, R, v, x, maxIter, ntol)
 % Newton's method for the tensor problem 
 % [I - alpha*R*(kron(x_n, I) + kron(I, x_n))]*x_(n+1) = ...
 %  -alpha*R*kron(x_n,x_n) + (1-alpha)*v
 % kappa records the ratio of ||x^(k+1) - x^(k)||_1 / ||x^(k) - x^(k-1)||_1
-tol = 1e-12;
-niter = 10000;
+if nargin < 5
+    niter = 1e4;
+    tol = 1e-12;
+else
+    niter = maxIter;
+    if nargin < 6
+        niter = maxIter;
+    else
+        tol = ntol;
+    end
+end
+
 xhist = zeros(size(x,1), niter);
 kappa = zeros(niter - 2, 1);
 xcur = x;
