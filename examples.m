1940dfd29396fb920e3f6cd6e15fa1987f50afd9
yuyongyang800 <yuyongyang800@gmail.com>
2014-01-09 14:37:42 -0500

    fix a bug

--- examples.m
+++ examples.m
@@ -1,8 +1,14 @@
 %% Run the power method of Li and Ng on a PageRank modified tensor
-load ling1 % load the first example for li and ng
-[~,R] = tensorpr3(P,1).markov();
-x0 = ones(size(R,1))/size(R,1);
-x = x0;
+load('./test_case/ling1') % load the first example for li and ng
+tpr3 = tensorpr3(P, 1);
+[~,R] = tpr3.markov();
+x0 = ones(size(R,1), 1)/size(R,1);
+xt = x0;
 for i=1:100
-    x = R*kron(x,x);
+    x = R*kron(xt,xt);
+    if norm(x - xt, 1) < 1e-8
+        break;
+    else
+        xt = x;
+    end
 end
