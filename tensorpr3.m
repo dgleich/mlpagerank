7d7e5e0242523b65e5495f3d66276a7951b43be0
yuyongyang800 <yuyongyang800@gmail.com>
2014-01-17 00:06:52 -0500

    remove w

--- tensorpr3.m
+++ tensorpr3.m
@@ -310,7 +310,7 @@ classdef tensorpr3
                 tmp = zeros(n^2, n);
                 for j = 1:n % column j
                     ej = zeros(n, 1);
-                    ej(j) = 1;w
+                    ej(j) = 1;
                     tmp(:, j) = kron(ej, MR(:, (i-1)*n + j));
                 end
                 P(:, (i-1)*n +1: i*n) = tmp;
