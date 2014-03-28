b54900d2f7845c3882828a6d08080ca5b74fbb5f
David Gleich <dgleich@purdue.edu>
2014-03-28 17:38:27 -0400

    New study that found a bug in shifting

---
+++ experiments/shiftstudy/shiftstudy_R4_1.m
@@ -0,0 +1,20 @@
+%% Check if R4_1 converges for any shift.
+
+load ../../tensors/mtm4
+
+R = R4_1;
+n = size(R,1);
+alpha = 0.99;
+tpr = tensorpr3(R,alpha);
+ng = 100;
+gammas = linspace(0,1,ng+2);
+gammas = gammas(2:end-1); % remove end points
+convflag = zeros(ng,1);
+
+for i=1:ng
+    [x,hist,flag] = tpr.shifted(gammas(i),'maxiter',1000);
+    convflag(i) = 1;
+end
+plot(convflag)
+    
+%% Nope!
